a64edecce2ccd75d6a820d291fefe27adada0d3d
mwinamijr <mwinamijr@gmail.com>
2024-11-23 09:03:55 +0300

    configured database to use postgresql

--- app/database.py
+++ app/database.py
@@ -1,14 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config import settings
 
-DATABASE_URL = "postgresql://username:password@localhost:5432/mydatabase"
+# Database URL from environment settings
+DATABASE_URL = settings.DATABASE_URL
 
+# SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
+
+# SessionLocal for creating database sessions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
 Base: DeclarativeMeta = declarative_base()
 
 
+# Dependancy for databse session
 def get_db():
     db = SessionLocal()
     try:
